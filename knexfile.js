12ce6387d1e1cade5f104a45d7466f900d300081
kneerace <kneerace@gmail.com>
2022-08-16 18:47:29 -0400

    migration update to knesfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,14 @@
 // Update with your config settings.
+const path = require("path");
 require("dotenv").config();
 const { DATABASE_URL } = process.env;
 
 module.exports = {
   development: {
     client: 'postgresql',
-    connection: DATABASE_URL, 
+    connection: DATABASE_URL,
+    migrations: {
+	directory: path.join(__dirname, "src", "db", "migrations"),
+	}, 
   },
 };
