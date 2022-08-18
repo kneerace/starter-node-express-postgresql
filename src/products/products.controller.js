7e59fc413c6355d5863ca6c4d0544b7b9969c60e
kneerace <kneerace@gmail.com>
2022-08-17 21:38:49 -0400

    products.serice.js: file added and updated for list to show all products from products table

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -1,14 +1,18 @@
+const productsService = require("./products.service");
+
+function list(req, res, next){
+  productsService
+    .list()
+    .then((data)=> res.json({data}))
+    .catch(next);
+}
+
 function read(req, res, next) {
   res.json({ data: { product_title: "some product title" } });
 }
 
-function list(req, res, next) {
-  res.json({
-    data: [{ product_title: "product 1" }, { product_title: "product 2" }],
-  });
-}
 
 module.exports = {
-  read: [read],
-  list: [list],
+  list,
+  read, 
 };
---
+++ src/products/products.service.js
@@ -0,0 +1,10 @@
+const knex = require("../db/connection");
+
+function list(){
+    return knex("products").select("*");
+}
+
+
+module.exports={
+    list,
+}
