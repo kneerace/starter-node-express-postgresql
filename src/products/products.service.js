97341621761d84bd9992086d998b31b96594d735
kneerace <kneerace@gmail.com>
2022-08-17 21:42:56 -0400

    products.service.js:read function added to pull specific records from product table

--- src/products/products.service.js
+++ src/products/products.service.js
@@ -4,7 +4,13 @@ function list(){
     return knex("products").select("*");
 }
 
+function read(productId){
+    return knex("products").select("*")
+        .where({product_id : productId})
+        .first(); // returns first row in the table as an object
+}
 
 module.exports={
     list,
+    read
 }
