1dba968c24c300f897e3ea019461994c6f2fd5c0
kneerace <kneerace@gmail.com>
2022-08-18 03:49:39 -0400

    suppliers.controller.js: validation function added for create

--- src/suppliers/suppliers.controller.js
+++ src/suppliers/suppliers.controller.js
@@ -1,5 +1,32 @@
 const supplierService = require("./suppliers.service");
 
+const VALID_PROPERTIES =[
+  "supplier_name",
+  "supplier_address_line_1",
+  "supplier_address_line_2",
+  "supplier_city",
+  "supplier_state",
+  "supplier_zip",
+  "supplier_phone",
+  "supplier_email",
+  "supplier_notes",
+  "supplier_type_of_goods",
+];
+
+function hasOnlyValidProperties(req, res, next){
+  const{data={}} = req.body;
+  const invalidFields = Object.keys(data).filter(
+    (field)=>!VALID_PROPERTIES.includes(field));
+
+  if(invalidFields.length){
+    return next({
+      status:400, 
+      message:`Invalid fields(s): ${invalidFields.join(", ")}`
+    });
+  }
+  next();
+}
+
 function list(req, res, next){
   return supplierService
           .list()
